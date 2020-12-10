08266965346e47b0b1798b6db27628bee089f5c5
Pengyuan Bian <bianpengyuan@google.com>
2020-12-10 17:33:08 +0000

    Remove null plugin mode from scaffold extension.
    Signed-off-by: Pengyuan Bian <bianpengyuan@google.com>

--- extensions/scaffold/plugin.h
+++ extensions/scaffold/plugin.h
@@ -1,16 +1,4 @@
-#ifndef NULL_PLUGIN
-
 #include "proxy_wasm_intrinsics.h"
-
-#else
-
-#include "include/proxy-wasm/null_plugin.h"
-
-namespace proxy_wasm {
-namespace null_plugin {
-namespace scaffold {
-
-#endif
 
 // PluginRootContext is the root context for all streams processed by the
 // thread. It has the same lifetime as the VM and acts as target for
@@ -33,11 +21,3 @@ class PluginContext : public Context {
     return dynamic_cast<PluginRootContext*>(this->root());
   }
 };
-
-#ifdef NULL_PLUGIN
-
-}  // namespace scaffold
-}  // namespace null_plugin
-}  // namespace proxy_wasm
-
-#endif
