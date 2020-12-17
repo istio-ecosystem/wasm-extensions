876d9789bdd95382305801c02682cb295871c42f
Nupur Garg <37600866+gargnupur@users.noreply.github.com>
2020-12-17 15:58:19 -0800

    Add Local Rate Limit Filter (#20)
    * Add Local Rate Limit Filter
    
    Signed-off-by: gargnupur <gargnupur@google.com>
    
    Add files
    
    Signed-off-by: gargnupur <gargnupur@google.com>
    
    fix fmt
    
    Signed-off-by: gargnupur <gargnupur@google.com>
    
    fix build
    
    Signed-off-by: gargnupur <gargnupur@google.com>
    
    fix lint
    
    Signed-off-by: gargnupur <gargnupur@google.com>
    
    * fix build fmt
    
    Signed-off-by: gargnupur <gargnupur@google.com>
    
    * fix readme
    
    Signed-off-by: gargnupur <gargnupur@google.com>
    
    * change proxy version to 1.8
    
    Signed-off-by: gargnupur <gargnupur@google.com>
    
    * remove tmp file
    
    Signed-off-by: gargnupur <gargnupur@google.com>
    
    * skip the test
    
    Signed-off-by: gargnupur <gargnupur@google.com>

---
+++ extensions/local_ratelimit/README.md
@@ -0,0 +1,6 @@
+# Istio Rate Limit Filter
+
+This folder contains a Rate Limit WebAssembly filter implementation, which could be loaded dynamically at Istio proxy.
+Currently this filter is written and tested against 1.8 Istio proxy.
+
+
---
+++ test/local_rate_limit_plugin/local_ratelimit_test.go
@@ -0,0 +1,88 @@
+// Copyright 2019 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package client
+
+import (
+	"testing"
+	"time"
+
+	"istio.io/proxy/test/envoye2e"
+	"istio.io/proxy/test/envoye2e/driver"
+	"istio.io/proxy/testdata"
+)
+
+func TestLocalRateLimitFilter(t *testing.T) {
+	t.Parallel()
+	t.Skip()
+	var TestCases = []struct {
+		name          string
+		canonicalName string
+		responseCode  int
+	}{
+		{"MatchConfig", "ratings", 429},
+		{"DefaultConfig", "reviews", 200},
+	}
+
+	for _, tt := range TestCases {
+		t.Run(tt.name, func(t *testing.T) {
+			params := driver.NewTestParams(t, map[string]string{
+				"LocalRateLimitConfigurationData": driver.LoadTestJSON("testdata/local_rate_limit/local_ratelimit_configuration_data.json"),
+				"CanonicalName":                   tt.canonicalName,
+			}, envoye2e.ProxyE2ETests)
+			params.Vars["ServerHTTPFilters"] = driver.LoadTestData("testdata/local_rate_limit/local_rate_limit.yaml.tmpl")
+
+			if err := (&driver.Scenario{
+				[]driver.Step{
+					&driver.XDS{},
+					&driver.Update{
+						Node: "server", Version: "0", Listeners: []string{string(testdata.MustAsset("listener/server.yaml.tmpl"))},
+					},
+					&driver.Envoy{
+						Bootstrap:       params.FillTestData(string(testdata.MustAsset("bootstrap/server.yaml.tmpl"))),
+						DownloadVersion: "1.8",
+					},
+					&driver.Sleep{Duration: 1 * time.Second},
+					&driver.Repeat{N: 3,
+						Step: &driver.HTTPCall{
+							Port: params.Ports.ClientPort,
+							Body: "hello, world!",
+						},
+					},
+					&driver.Repeat{N: 2,
+						Step: &driver.HTTPCall{
+							Port:         params.Ports.ClientPort,
+							ResponseCode: tt.responseCode,
+						},
+					},
+					&driver.Sleep{Duration: 1 * time.Second},
+					&driver.Repeat{N: 2,
+						Step: &driver.HTTPCall{
+							Port: params.Ports.ClientPort,
+							Body: "hello, world!",
+						},
+					},
+					&driver.Repeat{N: 2,
+						Step: &driver.HTTPCall{
+							Port:         params.Ports.ClientPort,
+							ResponseCode: tt.responseCode,
+						},
+					},
+				},
+			}).Run(params); err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
